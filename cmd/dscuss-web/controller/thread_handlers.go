b625be8b62f93708a00eb7c7efc1f123aaa99215
Vitaly Minko <vitaly.minko@gmail.com>
2019-05-31 07:26:48 +0300

    Implemented thread listing in the Web UI.

---
+++ cmd/dscuss-web/controller/thread_handlers.go
@@ -0,0 +1,124 @@
+/*
+This file is part of Dscuss.
+Copyright (C) 2019  Vitaly Minko
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package controller
+
+import (
+	"net/http"
+	"regexp"
+	"time"
+	"vminko.org/dscuss"
+	"vminko.org/dscuss/cmd/dscuss-web/view"
+	"vminko.org/dscuss/entity"
+	"vminko.org/dscuss/log"
+	"vminko.org/dscuss/thread"
+)
+
+type Thread struct {
+	ID            string
+	Topic         string
+	Subject       string
+	Text          string
+	DateWritten   string
+	AuthorName    string
+	AuthorID      string
+	AuthorShortID string
+	Replies       []Reply
+}
+
+type Reply struct {
+	ID            string
+	Subject       string
+	Text          string
+	DateWritten   string
+	AuthorName    string
+	AuthorID      string
+	AuthorShortID string
+}
+
+type ThreadComposer struct {
+	t *Thread
+	l *dscuss.LoginHandle
+}
+
+func (tc *ThreadComposer) Handle(n *thread.Node) bool {
+	m := n.Msg
+	if m == nil {
+		return true
+	}
+	if n.IsRoot() {
+		tc.t.ID = m.ID().String()
+		tc.t.Topic = m.Topic.String()
+		tc.t.Subject = m.Subject
+		tc.t.Text = m.Text
+		tc.t.DateWritten = m.DateWritten.Format(time.RFC3339)
+		tc.t.AuthorID = m.AuthorID.String()
+		tc.t.AuthorShortID = m.AuthorID.Shorten()
+		tc.t.AuthorName = userName(tc.l, &m.AuthorID)
+	} else {
+		tc.t.Replies = append(tc.t.Replies, Reply{})
+		r := &tc.t.Replies[len(tc.t.Replies)-1]
+		r.ID = m.ID().String()
+		r.Subject = m.Subject
+		r.Text = m.Text
+		r.DateWritten = m.DateWritten.Format(time.RFC3339)
+		r.AuthorID = m.AuthorID.String()
+		r.AuthorShortID = m.AuthorID.Shorten()
+		r.AuthorName = userName(tc.l, &m.AuthorID)
+	}
+	return true
+}
+
+func threadHandler(w http.ResponseWriter, r *http.Request, l *dscuss.LoginHandle, s *Session) {
+	var validURI = regexp.MustCompile("^/thread(id=[a-zA-Z0-9/+=]{32})?$")
+	m := validURI.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return
+	}
+	idStr := r.FormValue("id")
+	var tid entity.ID
+	err := tid.ParseString(idStr)
+	if err != nil {
+		BadRequestHandler(w, r, idStr+" is not a valid entity ID.")
+		return
+	}
+	node, err := l.ListThread(&tid)
+	if err != nil {
+		log.Fatal("Can't list thread: " + err.Error() + ".")
+		return
+	}
+	var t Thread
+	tc := ThreadComposer{&t, l}
+	tvis := thread.NewViewingVisitor(&tc)
+	node.View(tvis)
+	view.Render(w, "thread.html", map[string]interface{}{
+		"Common":        readCommonData(r, s, l),
+		"ID":            t.ID,
+		"Topic":         t.Topic,
+		"Subject":       t.Subject,
+		"Text":          t.Text,
+		"DateWritten":   t.DateWritten,
+		"AuthorName":    t.AuthorName,
+		"AuthorID":      t.AuthorID,
+		"AuthorShortID": t.AuthorShortID,
+		"Replies":       t.Replies,
+	})
+}
+
+func MakeThreadHandler(l *dscuss.LoginHandle) http.HandlerFunc {
+	return makeHandler(threadHandler, l)
+}
---
+++ cmd/dscuss-web/view/thread.go
@@ -0,0 +1,78 @@
+/*
+This file is part of Dscuss.
+Copyright (C) 2019  Vitaly Minko
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package view
+
+const threadHTML = `
+{{ define "content" }}
+
+{{ if .Common.IsWritingPermitted }}
+<div class="btn-row">
+	<a class="link-btn" href="/reply?id={{ .ThreadID }}">Reply</a>
+</div>
+{{end}}
+
+<h2 id="title"><a href="/thread?id={{ .ThreadID }}">{{ .Subject }}</a></h2>
+in <a href="/board?topic={{ .Topic }}">{{ .Topic }}</a> 
+<div class="message-row">
+	<div class="message">{{ .Text }}</div>
+	<div class="dimmed message-underline">
+		by <a href="/user?u={{ .AuthorID }}">{{ .AuthorName }}-{{ .AuthorShortID }}</a> {{ .DateWritten }}
+		{{ if .Common.IsWritingPermitted }}
+		| <a href="/ban?id={{ .AuthorID }}">ban</a>
+		| <a href="/rmmsg?id={{ .ID }}">delete</a>
+		| <a href="/reply?id={{ $.ID }}">reply</a>
+		{{ end }}
+	</div>
+</div>
+
+{{ if .Replies }}
+{{ range .Replies }}
+<hr class="sep">
+<div class="message-row" id="message-{{ .ID }}">
+	<b>{{ .Subject }}</b>
+	<div class="message">{{ .Text }}</div>
+	<div class="dimmed message-underline">
+		by <a href="/user?u={{ .AuthorID }}">{{ .AuthorName }}-{{ .AuthorShortID }}</a>
+		<a href="/lsop?id={{ .ID }}">{{ .DateWritten }}</a>
+		{{ if $.Common.IsWritingPermitted }}
+		| <a href="/ban?id={{ .AuthorID }}">ban</a>
+		| <a href="/rmmsg?id={{ .ID }}">delete</a>
+		| <a href="/reply?id={{ $.ID }}">reply</a>
+		{{ end }}
+	</div>
+</div>
+{{ end }}
+{{ else }}
+<div class="row">
+	<div class="dimmed">No replies to show.</div>
+</div>
+{{ end }}
+<div id="comment-last"></div>
+
+{{ if .Common.IsWritingPermitted }}
+<div style="margin-top: 40px;">
+<form action="/reply" method="POST" enctype="multipart/form-data">
+	<input type="hidden" name="csrf" value="{{ .Common.CSRF }}">
+	<input type="hidden" name="id" value="{{ .ThreadID }}">
+	<textarea name="content" rows="12" placeholder="Your reply..."></textarea>
+	<input type="submit" name="action" class="no-double-post" value="Post reply">
+</form>
+</div>
+{{ end }}
+
+{{ end }}`
